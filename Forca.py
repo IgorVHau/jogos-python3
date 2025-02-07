81debe8740e74940fe3788cb0fd2abf7016c6e38
IgorVHau <89521633+IgorVHau@users.noreply.github.com>
2025-02-07 15:50:21 -0300

    Preparando o jogo da forca - 07/02/2025

--- Forca.py
+++ Forca.py
@@ -2,6 +2,26 @@
 
 def jogar():
     print('*** Bem vindo ao jogo da letra ***')
-    print('JOGÃO!')
-    print(' Fim do jogo! ')
+    # Constantes
+    palavra_secreta = "banana".upper()
+    enforcado = False
+    acerto = False
+
+    # Loop
+    while(not enforcado and not acerto):
+        palpite = input('Escolha uma letra: ').upper().strip()
+        print('Jogando...')
+
+        index = 0
+        for letra in palavra_secreta:
+            if(palpite == letra):
+                print('Encontrei a letra {1} na posição {0}.'.format(index,letra))
+            index += 1
+
+    print('Fim do jogo!')
+
+if __name__ == '__main__':
+    jogar()
+
+
 
