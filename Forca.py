6a95383b0b770738852dc5ac2a5839e3c404fd7d
IgorVHau <89521633+IgorVHau@users.noreply.github.com>
2025-02-09 23:57:45 -0300

    Estipulando tentativas e erros - 09/02/2025

--- Forca.py
+++ Forca.py
@@ -2,21 +2,38 @@
 
 def jogar():
     print('*** Bem vindo ao jogo da letra ***')
-    # Constantes
-    palavra_secreta = "banana".upper()
+    # Constantes/Variáveis
+    palavra_secreta = "python".upper()
+    letras_acertadas = ["_","_","_","_","_","_"]
     enforcado = False
     acerto = False
+    erro = 0
 
     # Loop
     while(not enforcado and not acerto):
+        print(letras_acertadas)
         palpite = input('Escolha uma letra: ').upper().strip()
-        print('Jogando...')
 
-        index = 0
-        for letra in palavra_secreta:
-            if(palpite == letra):
-                print('Encontrei a letra {1} na posição {0}.'.format(index,letra))
-            index += 1
+        if(palpite in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(palpite == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erro += 1
+
+        #Condição para o enforcado = True
+        enforcado = erro >= 6
+
+        #Condição para o acerto = True
+        acerto = "_" not in letras_acertadas
+
+        #Mensagens de jogo encerrado
+        if(enforcado):
+            print('Você perdeu!')
+        if(acerto):
+            print('Você ganhou!')
 
     print('Fim do jogo!')
 
