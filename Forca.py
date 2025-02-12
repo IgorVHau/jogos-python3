c88de0a2fdb481e2b2ef0ee5a7a13f11f6ea13b0
IgorVHau <89521633+IgorVHau@users.noreply.github.com>
2025-02-11 21:20:56 -0300

    Automação do número de letras da palavra - 11/02/2025

--- Forca.py
+++ Forca.py
@@ -4,14 +4,21 @@ def jogar():
     print('*** Bem vindo ao jogo da letra ***')
     # Constantes/Variáveis
     palavra_secreta = "python".upper()
-    letras_acertadas = ["_","_","_","_","_","_"]
+    #letras_acertadas = ["_","_","_","_","_","_"]
+    letras_acertadas = ["_" for letra in palavra_secreta]
     enforcado = False
     acerto = False
     erro = 0
+    tentativa = 6
 
     # Loop
     while(not enforcado and not acerto):
         print(letras_acertadas)
+        if tentativa - erro == 1:
+            print('Você só tem mais {} tentativa.'.format(tentativa - erro))
+        else:
+            print('Você tem {} tentativas.'.format(tentativa - erro))
+        #print('Número de tentativa(s): {}'.format(tentativa - erro))
         palpite = input('Escolha uma letra: ').upper().strip()
 
         if(palpite in palavra_secreta):
@@ -24,7 +31,7 @@ def jogar():
             erro += 1
 
         #Condição para o enforcado = True
-        enforcado = erro >= 6
+        enforcado = erro == tentativa
 
         #Condição para o acerto = True
         acerto = "_" not in letras_acertadas
